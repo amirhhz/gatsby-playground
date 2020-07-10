58c6fd4a03e8f15f35951a71350a6bb672de2954
Amir H. Hajizamani <amirhhz@users.noreply.github.com>
2020-07-10 17:40:48 +0100

    Include richer metadata on posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 import Layout from "../components/layout"
 
 export default function BlogPost({ data }) {
@@ -8,6 +9,8 @@ export default function BlogPost({ data }) {
     <Layout>
       <div>
         <h1>{post.frontmatter.title}</h1>
+        <pre css={css`float: right`} title={post.frontmatter.date}>Written: {post.frontmatter.ago}</pre>
+        <pre title={post.wordCount.words}>Time to read: {post.timeToRead}</pre>
         <article dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </Layout>
@@ -20,6 +23,12 @@ export const query = graphql`
       html
       frontmatter {
         title
+        ago: date(fromNow: true)
+        date
+      }
+      timeToRead
+      wordCount {
+        words
       }
     }
   }
